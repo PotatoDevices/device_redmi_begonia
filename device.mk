0d57b28bc078bc2e262f451a31326e221106a095
Bruno Martins <bgcngm@gmail.com>
2018-09-22 00:06:31 +0100

    begonia: Switch to NQ NFC stack
     * So that it matches prebuilt vendor. On a sidenote, this was
       moved from the common tree to the individual device trees,
       because not all the targets support NFC.
    
    Change-Id: Ic600594bc073ef3883225fb01039b1953a880807
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>

--- device.mk
+++ device.mk
@@ -91,6 +91,18 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
 
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    libnqnfc_nci_jni \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
+    NQNfcNci \
+    SecureElement \
+    Tag
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
