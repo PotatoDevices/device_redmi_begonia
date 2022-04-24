a7430e40f5253d51ba12c3c879c83ea5aef99ae8
SahilSonar <sss.sonar2003@gmail.com>
2022-04-24 06:48:50 +0200

    begonia: sepolicy: Drop ro.product.system.manufacturer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,9 +147,9 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Sepolicy
 include device/mediatek/sepolicy/sepolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Inherit from the proprietary version
 -include vendor/redmi/begonia/BoardConfigVendor.mk
