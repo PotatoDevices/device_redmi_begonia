d90388b9925f931fd1bdd4a90344995b9563d83f
TheMalachite <eliasgheeraert@gmail.com>
2022-09-07 19:56:53 +0200

    begonia: Patchelf AIDL/HIDL power service to load ndk backend instead of ndk_platform backend

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,12 @@ function blob_fixup {
         lib/libsink.so)
             "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
             ;;
+        vendor/bin/hw/power_native_test_v_1_1)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
+            ;;
+        vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.lights-service.mediatek)
             "${PATCHELF}" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
             ;;
@@ -72,6 +78,9 @@ function blob_fixup {
             ;;
         vendor/lib64/hw/audio.primary.mt6785.so)
             "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
+            ;;
+        vendor/lib64/android.hardware.power-service-mediatek.so)
+            "${PATCHELF}" --replace-needed "android.hardware.power-V1-ndk_platform.so" "android.hardware.power-V1-ndk.so" "${2}"
             ;;
         vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
